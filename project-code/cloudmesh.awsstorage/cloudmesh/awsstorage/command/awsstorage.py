ea012f3d2e37a01efba4b8c16747b144bb523715
manjunathsivan <46654189+manjunathsivan@users.noreply.github.com>
2019-03-21 20:05:22 -0400

    update storage and manager

--- project-code/cloudmesh.awsstorage/cloudmesh/awsstorage/command/awsstorage.py
+++ project-code/cloudmesh.awsstorage/cloudmesh/awsstorage/command/awsstorage.py
@@ -1,49 +1,122 @@
 from __future__ import print_function
 from cloudmesh.shell.command import command
 from cloudmesh.shell.command import PluginCommand
-from cloudmesh.awsstorage.api.manager import Manager
+from cloudmesh.awsawsstorage.api.manager import Manager
 from cloudmesh.common.console import  Console
 from cloudmesh.common.util import path_expand
 from pprint import pprint
 
 
-class AwsstorageCommand(PluginCommand):
+class AwsawsstorageCommand(PluginCommand):
 
     # noinspection PyUnusedLocal
     @command
-    def do_awsstorage(self, args, arguments):
-        """
+    def do_awsawsstorage(self, args, arguments):
+      """
         ::
+        Usage:
+            awsstorage [--awsstorage=<SERVICE>] create dir DIRNAME
+            awsstorage [--awsstorage=<SERVICE>] delete dir DIRNAME
+            awsstorage [--awsstorage=<SERVICE>] list dir files [DIRNAME]
+            awsstorage [--awsstorage=<SERVICE>] put file SOURCEFILENAME SOURCEDIR DESTFILENAME DESTDIR
+            awsstorage [--awsstorage=<SERVICE>] gett file SOURCEFILENAME SOURCEDIR DESTFILENAME DESTDIR
+            awsstorage [--awsstorage=<SERVICE>] delete file FILENAME DIRNAME
+            awsstorage [--awsstorage=<SERVICE>] search file FILENAME [DIRNAME]
+            awsstorage [--awsstorage=<SERVICE>] list file info FILENAME DIRNAME
 
-          Usage:
-                awsstorage --file=FILE
-                awsstorage list
+        Manage file awsstorage on AWS S3 buckets and perform operations like put, get, delete on the files.
 
-          This command does some useful things.
+        Arguments:
+            DIRNAME Name of the directory where file is to be created or searched or deleted.
+            FILENAME Name of the file is to be created or searched or deleted.
+            SOURCEFILENAME Name of the source file for put or get actions
+            SOURCEDIR Name of the source file directory for put or get actions
+            DESTFILENAME Name of the destination file for put or get actions
+            DESTDIR Name of the destination file directory for put or get actions
 
-          Arguments:
-              FILE   a file name
+        Options:
+          -h --help
+          --awsstorage=<SERVICE>  Cloud awsstorage service name like aws or azure or box or google
 
-          Options:
-              -f      specify the file
+        Description:
+            Commands to manage file awsstorage on cloud
 
+            awsstorage create dir
+                Creates directory with the given name.
+
+            awsstorage delete dir
+                Deletes directory with the given name.
+
+            awsstorage list dir files
+                Lists all files present in the input directory.
+                If no dir is specified, it will list all files across directories.
+
+            awsstorage put file
+                Uploads file to cloud awsstorage from the local store.
+
+            awsstorage gett file
+                Downloads file from cloud awsstorage to the local store.
+
+            awsstorage delete file
+                Deletes the input file from specified cloud awsstorage directory.
+
+            awsstorage search file
+                Searches and lists the input file from specified cloud awsstorage directory.
+                If no dir is specified, it will list all files across directories which match the filename.
+
+            awsstorage list file info
+                Lists the file attributes for the input file.
+
+        Example:
+            set awsstorage=aws
+            awsstorage put FILENAME DESTDIR
+
+            is the same as
+            awsstorage  --awsstorage=aws put FILENAME DESTDIR
         """
-        arguments.FILE = arguments['--file'] or None
 
-        print(arguments)
+        pprint(arguments)
 
         m = Manager()
 
+        service = None
 
-        if arguments.FILE:
-            print("option a")
-            m.list(path_expand(arguments.FILE))
+        #filename = arguments.FILENAME[0]
+        try:
+            service = arguments["--awsstorage"]
+        except Exception as e:
+            try:
+                v = Variables()
+                service = v['awsstorage']
+            except Exception as e:
+                service = None
 
-        elif arguments.list:
-            print("option b")
-            m.list("just calling list without parameter")
+        if service is None:
+            Console.error("awsstorage service not defined")
 
-
-        Console.error("This is just a sample")
-        return ""
+        if arguments.create == True and arguments.dir == True:
+            m.createDir(service, arguments.DIRNAME)
+        elif arguments.gett == True and arguments.file == True:
+            print('In service get file')
+            m.getFile(service, arguments.SOURCEFILENAME, arguments.SOURCEDIR, arguments.DESTFILENAME, arguments.DESTDIR)
+        elif arguments.delete == True and arguments.dir == True:
+            m.deleteDir(service, arguments.DIRNAME)
+        elif arguments.list == True and arguments.dir == True and arguments.files == True:
+            if arguments.DIRNAME is not None:
+                m.listDirFiles(service, arguments.DIRNAME)
+            else:
+                m.listDirFiles(service, '')
+        elif arguments.put == True and arguments.file == True:
+            m.putFile(service, arguments.SOURCEFILENAME, arguments.SOURCEDIR, arguments.DESTFILENAME, arguments.DESTDIR)
+        elif arguments.delete and arguments.file == True:
+            m.deleteFile(service, arguments.FILENAME, arguments.DIRNAME)
+        elif arguments.search == True and arguments.file == True:
+            if arguments.DIRNAME is not None:
+                m.searchFile(service, arguments.FILENAME, arguments.DIRNAME)
+            else:
+                m.searchFile(service, arguments.FILENAME, '')
+        elif arguments.list == True and arguments.file == True and arguments.info == True:
+            m.listFileInfo(service, arguments.FILENAME, arguments.DIRNAME)
+        else:
+            print("Command not recognized.")
 
